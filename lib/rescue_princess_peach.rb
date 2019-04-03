65b3284f7d4a2034fde5e46212d7c7c8d258a1a8
Eric Bodden <ericbodden30@gmail.com>
2019-04-03 21:48:57 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -8,8 +8,8 @@ end
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
-# puts status
+# rescue_princess_peach = "Hooray! Mario has rescued Princess Peach."
+# puts status = rescued
 
 
 
